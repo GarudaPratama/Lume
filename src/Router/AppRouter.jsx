6c55ece3776909054551d37d45233bf281195307
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-10-23 13:29:34 +0700

    Lume

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,30 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import LandingPage from "../pages/LandingPage";
 import AppPage from "../pages/AppPage";
 import Login from "../components/auth/Login";
 import SignUp from "../components/auth/SignUp";
-import PrivateRoute from "../components/auth/PrivateRoute";
 
 export default function AppRouter() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/app"
-          element={
-            <PrivateRoute>
-              <AppPage />
-            </PrivateRoute>
-          }
-        />
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><LandingPage /></PageTransition>} />
+        <Route path="/signup" element={<PageTransition><SignUp /></PageTransition>} />
+        <Route path="/login" element={<PageTransition><Login /></PageTransition>} />
+        <Route path="/app" element={<PageTransition><AppPage /></PageTransition>} />
         <Route
           path="*"
-          element={<div className="text-center mt-32 text-xl">404 - Page Not Found</div>}
+          element={<PageTransition><div className="text-center mt-32 text-xl">404 - Page Not Found</div></PageTransition>}
         />
       </Routes>
-    </BrowserRouter>
+    </AnimatePresence>
+  );
+}
+
+// Component animasi halaman
+function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
   );
 }
