3c5c6d28ae0a378215410d0af91764b00209bf82
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-10-23 11:26:49 +0700

    Lume

---
+++ src/auth/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/config";
+
+export default function PrivateRoute({ children }) {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <div className="text-center mt-32">Loading...</div>;
+
+  if (!user) return <Navigate to="/" replace />;
+
+  return children;
+}
